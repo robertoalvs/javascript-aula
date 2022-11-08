ab4c99469edb5e2e461a959ff7e50ee0c642b8ef
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-08 19:05:40 -0300

    atualização javascript

---
+++ aula6/js/index.js
@@ -0,0 +1,22 @@
+
+// let Nome = 'Roberto'; // let = variável ---- Nome = 'Roberto' = String 
+
+// console.log(Nome,'está estudando javascript');
+// console.log('Quando o', Nome, 'termina esse curso?');
+// console.log('Tudo vai dar certo para o', Nome);
+// console.log('Ele vai ser tornar um ótimo programador quem? O', Nome); // String = Nome
+
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar um nome de uma variável com número
+// As variáveis não pode conter espaços ou traços
+// Utilizamos camelcase
+// Case-sensitive
+// Não podemos recriar uma variável com a palavra let.
+
+let nome = 'Roberto';
+nomeSobrenome = 'Dos Santos Alves';
+idade = '40';
+
+console.log(nome, nomeSobrenome, idade);
