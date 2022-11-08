a3da6bb9e9db7d2487502a1cb885bea45103c4e7
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-08 14:25:47 -0300

    atualização javascript

---
+++ aula3/app.js
@@ -0,0 +1,7 @@
+console.log('Hello world!'); // String = TEXTO
+console.log(`"Roberto" 'alves' `);
+console.log("roberto Alves");
+console.log(35, 15.85,); // Number = Número
+
+
+console.log('Meu nome é "Roberto Alves." Estou aprendendo JavaScript às', 10,'da manhã.')
