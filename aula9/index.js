ca1f9906d806898619ac5ad3d752217128a934e8
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-09 18:23:40 -0300

    atualizado js

---
+++ aula9/index.js
@@ -0,0 +1,16 @@
+//ECMAscript  - em 2015 (ES6) Quando foi adicionado a funçào LET
+
+//var nome = 'Roberto';
+// var nome = 'João';   // Com VAR você pode redeclarar variável
+
+// console.log (nome);
+
+let nome = 'Roberto';
+nome = 'Paulo';
+
+console.log(nome);
+
+//nome = 'Douglas';  // Com LET não podemos redeclar variável
+// O correto para utilizar o LET e redeclarar uma variável
+
+
