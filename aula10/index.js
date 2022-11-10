3d90c8dfdaef2a79a1eb4eff955b13ce8797f238
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-10 14:19:53 -0300

    atualização javascript parseInt parseFloat Number NaN

---
+++ aula10/index.js
@@ -0,0 +1,72 @@
+/* ===============================================================
+
+Aritméticos
+
++ ADIÇÃO / CONCATENAÇÃO
+- SUBITRAÇÃO
+/ DIVISÃO
+* MULTIPLICAÇÃO 
+
+++ INCREMENTO
+-- DECREMENTO
+** Potenciação
+
+% Resto da divisão
+
+
+================================================================  */
+
+
+/*
+
+const num1 = 10; // Quando adicionamos ASPAS em números, se torna STRING
+const num2 = 3; // NORMAL NUMBER
+
+console.log(num1 % num2); 
+
+*/
+
+
+/*
+const num1 = 5;
+const num2 = 10;
+
+console.log(num1 + num2); */
+
+/*
+
+let contador = 5;
+contador++ ; // 6
+contador++ ; // 7
+contador++ ; // 8
+contador++ ; // 9
+
+console.log(contador); */
+
+
+/*==============OPERADOR DE INCREMENTO================================*/
+
+/* let contador =1;
+console.log(++contador);    //PRÉ
+console.log(contador); */
+
+/* let contador =1;
+console.log(contador++);  // PÓS
+console.log(contador);
+
+*/
+
+
+/*==============OPERADOR DE DECREMENTO================================*/
+
+/* let contador =1;
+console.log(--contador);    //PRÉ
+console.log(contador); */
+
+ /* let contador = 1;
+console.log(contador--);  // PÓS
+console.log(contador); */
+
+let contador = 2;
+contador **= 10;
+console.log(contador);
