e39a44b43f5fc2e8b59343d824657ddfeb02eb2f
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-14 16:04:45 -0300

    atualizacão js

---
+++ aula11/java/index.js
@@ -0,0 +1,14 @@
+/* alert('Estudando Javascript');
+confirm('Deseja Realmente Apagar?');
+prompt('Digite o seu nome'); */
+
+let num1 = ('400');
+let num2 = ('300');
+ 
+num1 = parseFloat(num1);
+
+num2 = Number(num2);
+
+console.log (num1 + num2);
+
+typeof num1, num2;
