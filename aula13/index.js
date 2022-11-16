94690fe4f5cc31319bd9220d40093f8db8dcfcc1
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-16 14:30:19 -0300

    atualização java script

--- aula13/index.js
+++ aula13/index.js
@@ -3,10 +3,18 @@
 // Strings
 
 
-let roberto = prompt('Digite seu Primeiro Nome');
+/* let roberto = prompt('Digite seu Primeiro Nome');
 let robertoS = prompt('digite seu segundo Nome');
 
 const resultado = 'roberto, robertoS';
 
-alert (roberto + robertoS);
+alert (roberto +  robertoS); */
+
+
+function moveon() {
+
+    let pergunta = confirm("Ready to move on?");
+    if (pergunta) window.location = "https://google.com";
+    setTimeout(moveon, 60000);
+}
 
