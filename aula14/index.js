b2c65dab230c9324c38426b2ac5ce4e18c572b2a
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-23 14:25:18 -0300

    atualizado javascript number

---
+++ aula14/index.js
@@ -0,0 +1,65 @@
+/* let num1 = 1;
+let num2 = 2.5;
+console.log(num1 + num2); */
+
+
+/* let num1 = 1;
+let num2 = 2.5;
+console.log(num1.toString() + num2); // Converte um numero em string, assim o num1 + num2 ficou = 12.5 por que foi concatenado.
+console.log(typeof num1); */
+
+
+/* let num1 = 1;
+let num2 = 2.5;
+console.log(num1.toString() + num2); // Converte um numero em string, assim o num1 + num2 ficou = 12.5 por que foi concatenado. */
+
+
+/* let num1 = 1;
+let num2 = 2.5;
+num1 = num1.toString(); // Converte para String um number
+console.log(typeof num1); */ 
+
+/* let num1 = 1500;
+let num2 = 2.5;
+console.log(num1.toString(2)); // Esse codigo você consegue ver o número binario de 1500 */
+
+
+/* let num1 = 10.5789551255547;
+let num2 = 2.5;
+
+console.log(num1.toFixed(2)); // toFixed arredonda esses números para 2 casas decimais, ficando = 10.58 */
+
+
+// console.log(Number.isInteger(num1));// Retorna se é um número interiro ou não, como FALSE ou TRUE 
+
+//let temp = num1 * 'Ola';
+//console.log(temp); // retornou NaN, not a number
+//console.log(Number.isNaN(temp)); // Se essa conta for um Not a Number, vai retornar verdadeiro = True - se sua conta for inválida 
+
+//let temp = num1 * '5';
+//console.log(temp); // retornou NaN, not a number
+//console.log(Number.isNaN(temp)); // Se essa conta for um Not a Number, vai retornar verdadeiro = True - se sua conta for Válida 
+
+
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
