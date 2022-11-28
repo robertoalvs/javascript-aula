2510aa235827c7ca4f5af486a30b23bc8ee9de22
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-28 09:50:23 -0300

    atualizado javascript Math.floor - Math.ceil - Math.max e Math.min

---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1); // Math.floor - Arredonda o número para baixo.
+// let num2 = Math.ceil(num1); // Math.ceil - Arredonda o número para cima.
+// let num2 = Math.round(num1); // Arredonda para o número mais proximo, no exemplo era 9.54578, assim ficando 10. 
+
+// console.log(Math.max(1,2,3,4,5, -10, -50, 1500, 9,8,7,6)); // o (Math.max) vai procurar o maior número da sequência informada, que no caso é 1500.
+
+// console.log(Math.min(1,2,3,4,5, -10, -50, 1500, 9,8,7,6)); // o (Math.min) vai procurar o menor número da sequência informada, que no caso é -50.
+
+// console.log(Math.random()); // Vai buscar um número entre 0 e 1, assim gerando um número aleatório. 
+
+console.log(2 ** 10); // 2 elevado a 10
