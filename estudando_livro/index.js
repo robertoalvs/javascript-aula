f73487745a5937bff5ea96fe2b423951ae3a5ede
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-29 11:53:56 -0300

    atualizando javascript estudando o livro

---
+++ estudando_livro/index.js
@@ -0,0 +1,18 @@
+
+
+ /* let n =123456.789;
+
+//console.log(n.toFixed(0));
+//console.log(n.toFixed(2));
+//console.log(n.toFixed(5));
+//console.log(n.toExponential(1));
+//console.log(n.toExponential(3));
+//console.log(n.toPrecision(4));
+//console.log(n.toPrecision(7));
+console.log(n.toPrecision(10));
+                                    */
+
+
+let n =10;
+
+console.log(parseInt("ff", 16));
