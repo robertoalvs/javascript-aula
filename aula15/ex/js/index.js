9ee5ef1fb1175b10e0de99266c5eb15a63f2d2b4
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-30 14:44:05 -0300

    atualizado

--- aula15/ex/js/index.js
+++ aula15/ex/js/index.js
@@ -3,3 +3,4 @@ const numero = prompt('Digite um número:');
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
+numeroTitulo.innerHTML = numero;
