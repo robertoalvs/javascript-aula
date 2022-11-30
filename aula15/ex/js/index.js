e50360d639fd36d486ad72bd8994f75d611e3710
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-11-30 16:59:39 -0300

    atualização JavaScript interação com HTML

--- aula15/ex/js/index.js
+++ aula15/ex/js/index.js
@@ -1,6 +1,15 @@
 
-const numero = prompt('Digite um número:');
+const numero = Number (prompt('Digite um número:')); // PROMPT SEMPRE RETORNA UMA STRING, TEMOS QUE CONVERTER PARA Number
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
+
+texto.innerHTML = '';
+texto.innerHTML += `<p> Seu numero + 5 é: ${numero + 5}. </p>`;
+texto.innerHTML += `<p> Raiz quadrada é: ${numero ** 0.5}. </p>`;
+texto.innerHTML += `<p> É NaN: ${Number.isNaN(numero)}. </p>`;
+texto.innerHTML += `<p> ${numero} É inteiro: ${Number.isInteger(numero)}. </p>`;
+texto.innerHTML += `<p> Arredondando para baixo: ${Math.floor(numero)}. </p>`;
+texto.innerHTML += `<p> Arredondando para cima: ${Math.ceil(numero)}. </p>`;
+texto.innerHTML += `<p> Com duas casas decimais: ${numero.toFixed(2)}. </p>`;
