4d80367d6999072cf94e9128b07217966cd73596
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-12-01 15:10:54 -0300

    atualização

---
+++ aula16/index.js
@@ -0,0 +1,47 @@
+const alunos = ['Luiz', 'Maria', 'João']; // Arrays são listas de coisas
+const removido = alunos.pop(); // Exibi o ÚLTIMO elemento removido do array
+const removidos = alunos.shift(); // Exibi o PRIMERIO elemento removido do array
+
+alunos.unshift('Wagner'); // Para adicionar um elemento no COMEÇO da ARRAYS
+
+alunos.push('Otávio'); // Para adicionar um elemento no FINAL da ARRAYS
+alunos.push('Pedro'); // Para adicionar um elemento no FINAL da ARRAYS
+alunos.push('Douglas'); // Para adicionar um elemento no FINAL da ARRAYS
+
+alunos.pop(); // Remove o ÚLTIMO elemento do array
+alunos.shift(); // Remove o PRIMEIRO elemento do array
+delete alunos[0]; // Para APAGAR um elemento do array
+
+console.log(alunos); // Exibi a variável alunos
+console.log(removido); // Exibi a variável alunos removido
+console.log(removidos); // Exibi a variável removidos
+console.log(typeof alunos); // Exibi que tipo de elemento a variável alunos é.
+
+console.log(alunos instanceof Array) // Aqui ele pergunta se a variável alunos, é uma instância de Array
+
+
+/* console.log(alunos [5]);
+console.log(alunos [4]); */
+
+
+
+
+
+
+
+
+
+
+
+/* alunos[1] = 'Eduardo'; // Para editar um nome dentro de uma arryas, nesse caso estou alterando o nome "Maria" para "Eduardo"
+alunos [3] = 'Luiza'; // Assim adicionamos um elemento no arrays
+console.log (alunos);
+console.log (alunos[2]);
+console.log (alunos[1]); */
+
+// console.log(alunos.length); // Para saber o tamanho do ARRAY usamos o "length"
+
+/* const nome = 'Luiz Otavio';
+console.log(nome [1]); // Para saber qual é a letra de uma string 
+// Resposta u */
+
