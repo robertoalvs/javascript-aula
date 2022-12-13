196e0ce89ce0fb50ee181ecbadc42f785061dfe2
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-12-13 12:05:16 -0300

    atualização operadores JS

---
+++ curso-em-video/aula7/index.js
@@ -0,0 +1,62 @@
+
+/* let a = 5 + 3
+let b = a % 5 
+let c = 5 * b ** 2 // Primeiro faremos a conta de **  (b**2)
+let d = 10 - a / 2 // Primeiro faremos a conta a/2
+let e = 6 * 2 / d
+let f = b % e + 4 / e // Primeiro faremos a conta de b % e */
+
+
+// ***************** OPERADORES RALACIONAIS **************
+
+//          >
+//          <
+//          >=
+//          <=
+//          ==
+//          !=
+
+let a = 4;
+let b = 20;
+let c = 10
+
+console.log( a <= b - c);
+
+
+
+// ******************* INCREMENTO *********************
+
+/* 
+let n = 10;
+
+n++; // valor de n11
+n--; // valor de n10
+
+*/
+
+
+
+ //************ Auto ATRIBUIÇÃO *******************
+
+/* n = n + 4; // Fazer a conta depois do recebe =
+n = n - 5;
+n = n * 4;
+n = n / 2;
+n = n ** 2;
+n = n % 5; */
+
+/* // ******************* 
+n += 4;  // Só funciona se a mesma variável receber ela mesmo exemplo  (n = n) se fosse (n = x) não pode!
+n -= 5;
+n *= 4;
+n /= 2;
+n **= 2;
+n %= 5;
+
+console.log (n);
+                    */
+
+
+               
+
+
