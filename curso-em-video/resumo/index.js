bc483ebdcf83a8e52fdbe35cf66afd51a132b409
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-12-14 15:09:46 -0300

    atualização

--- curso-em-video/resumo/index.js
+++ curso-em-video/resumo/index.js
@@ -18,6 +18,12 @@
 // let d = 10 - a / 2; // OBS IMPORTANTE, na ordem de precedência fazer primeiro a divisão   = 6
 // let e = 6 * 2 / d;  // = 2let f = b % e + 4 / e;   // = 3
 
+
+
+
+
+//  ******************************************************************
+
 // let x = 5;
 
 // x++; // 6
@@ -26,8 +32,58 @@
 // x--
 // console.log (x);
 
-let n = 3;
-n = n + 4;  // = 7
-n = n - 5;  // = 2 
+//  *******************************************************************
 
-console.log(n);
+/* 
+let n = 10;
+n = n + 4; // n= 14
+n = n + 5; // n= 19
+
+n = n += 1; // n=20  SIMPLIFICANDO 
+n = n -=6; // = n14
+
+let num = 10;
+ num++;        // INCREMENTO
+ num++; // num = 12
+ num--; // num = 11 // DECREMENTO
+
+console.log(num);
+*/
+
+//  ********************************************************************
+
+/* 
+let maior = 5 > 2;
+let menor = 7 < 4;
+let maiorigual = 8 >= 8;     // TODA EXPRESSÃO QUE TEM UM OPERADOR RELACIONAL ... O RESULTADO SERÁ BOOLEANO - TRUE OR FALSE
+let menorigual = 9 <= 7;
+let igual = 5 == 5;
+let diferente = 10 != 6;  // 10 é diferente de 6 = true
+
+let a = 58;
+let b = 1250;
+
+let soma = b >= a + 10;  // a58 + 10 = (68) ********** Então b1250 é maior que (a + 10)  -- TRUE
+
+console.log(diferente);
+*/
+
+//  *******************************************************************
+
+/* 
+let igual = 5 == '5';  // 5 é igual a " String '5' " --- true
+let i = 5 == 5; //    5 é igual a 5 ------ true
+let ig = 5 === '5'// OPERADOR DE IDENTITADE RESTRITA = AQUI TEMOS O MESMO VALOR 5, POREM NÃO É IGUAL ----- FALSE 
+
+let j = 80 != 90 
+
+console.log(ig);
+
+*/
+
+
+let n1 = 10;
+let n2 = 20;
+let soma = n1 > n2 && n2 % 2 == 0;  // FALSE  - Quando temos operadores aritmeticos, 
+
+console.log(soma);
