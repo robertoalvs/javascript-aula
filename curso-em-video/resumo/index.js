d2f3004a3b5331708ad6958357cbe60f08263ae1
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-12-14 10:19:26 -0300

    atualização resumo js

---
+++ curso-em-video/resumo/index.js
@@ -0,0 +1,33 @@
+
+
+// let n = 200;
+// let n1 = 200;
+// let s = n + n1;
+
+        
+// console.log (typeof n );
+
+// *********************************************************
+
+// let n = 10;
+// let n1 = 5;
+// let soma = n % n1; // resultado 0
+
+// console.log (soma)
+// let a = 5 + 3; // = 8let b = a % 5; // = 3let c = 5 * b ** 2; // OBS IMPORTANTE, na ordem de precedência fazer primeiro ** a potência.    = 45
+// let d = 10 - a / 2; // OBS IMPORTANTE, na ordem de precedência fazer primeiro a divisão   = 6
+// let e = 6 * 2 / d;  // = 2let f = b % e + 4 / e;   // = 3
+
+// let x = 5;
+
+// x++; // 6
+// x++;
+// x++;
+// x--
+// console.log (x);
+
+let n = 3;
+n = n + 4;  // = 7
+n = n - 5;  // = 2 
+
+console.log(n);
