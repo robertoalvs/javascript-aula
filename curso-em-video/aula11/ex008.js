073a0c568850cec4783d4615899f69e8a1a27c75
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2022-12-23 11:04:00 -0300

    atualzação js if and else

---
+++ curso-em-video/aula11/ex008.js
@@ -0,0 +1,10 @@
+
+let vel = 78;
+console.log(`A velocidade do seu carro é ${vel}KM/h`);
+
+if (vel > 60){ // if é uma consdição SIMPLES. if == Se
+
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO!!!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ curso-em-video/aula11/ex009.js
@@ -0,0 +1,12 @@
+let país = 'BRASIL'
+
+console.log(`Vivendo em ${país}!!!!`)
+
+if (país == 'BRASIL') {
+
+    console.log(`Você é BRASILEIRO!`)
+}
+else {
+
+    console.log(`Você é ESTRANGEIRO!`)
+}
