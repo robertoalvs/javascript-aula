fc4d264d978ce331b53f4f2068c0916b9018eb29
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-01-07 10:53:12 -0300

    atualizado

--- curso-em-video/aula12/ex011.js
+++ curso-em-video/aula12/ex011.js
@@ -1,15 +1,32 @@
+ // let idade = 67
+
+// console.log(`Você tem ${idade} anos.`)
+
+// if (idade < 16){
+
+   //  console.log('Não Vota')
+
+// } else if (idade < 18 || idade > 65){
+
+       //  console.log('Voto opcional')
+
+// } else {
+    // console.log('Voto Obrigatório')
+// } 
+
+
 let idade = 67
+console.log (`Você tem ${idade} anos`)
 
-console.log(`Você tem ${idade} anos.`)
 
-if (idade < 16){
+if (idade < 16) {
 
-    console.log('Não Vota')
+    console.log('Não vota')
 
-} else if (idade < 18 || idade > 65){
+} else if (idade < 18 || idade > 65) {
 
-        console.log('Voto opcional')
-
+    console.log ('Voto opcional')
 } else {
-    console.log('Voto Obrigatório')
+
+    console.log ('Voto obrigatório')
 }
--- curso-em-video/aula12/ex012.js
+++ curso-em-video/aula12/ex012.js
@@ -63,6 +63,22 @@ switch (dia) {
     case 2:
         console.log('Terca')
         break
+
+     case 3:
+         console.log('Quarta')
+         break
+        
+     case 4:
+         console.log('Quinta')
+         break
+
+     case 5:
+         console.log('Sexta')
+        break
+
+     case 6:
+        console.log('Sábado')
+        break 
         
     default: 
         console.log('[erro] dia inválido')
