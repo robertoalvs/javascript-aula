b3cec227dc38ea5de4ebabbce09bcbb7a99d831c
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-01-14 23:23:46 -0300

    atualização

--- curso-em-video/resumo/teste-java/script.js
+++ curso-em-video/resumo/teste-java/script.js
@@ -1,5 +1,33 @@
 
 function checar() {
 
-window.alert(`Oi boa noite!`)
+let res = document.getElementById('res')
+let check = document.getElementsByName('box')
+
+let checkbox = ''
+
+    if(check[0].checked) {
+
+        checkbox = 'Futebol' 
+
+        res.innerHTML = `Seu esporte favorito é: <strong>${checkbox}</strong>`
+
+    } else if (check[1].checked) {
+
+        checkbox = 'Basquete'
+
+        res.innerHTML = `Seu esporte favorito é: <strong>${checkbox}</strong>`
+
+    } else if (check[2].checked) {
+
+        checkbox = 'Tênis'
+
+        res.innerHTML = `Seu esporte favorito é: <strong>${checkbox}</strong>`
+
+    } else {
+
+        alert('[ERRO!] Escolha uma opção.')
+    }
+    
+    
 }
