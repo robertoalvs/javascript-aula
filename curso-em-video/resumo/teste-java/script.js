5a56a9f01e99f4f5005fe9d932340567ff85a38a
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-01-14 22:34:04 -0300

    atualizado

--- curso-em-video/resumo/teste-java/script.js
+++ curso-em-video/resumo/teste-java/script.js
@@ -1,19 +1,5 @@
 
 function checar() {
 
-
-    let res = document.getElementById('res')
-    let check = document.getElementById('ifut')
-
-    let caixa = ''
-
-    
-    res.innerHTML `Você clicou em ${caixa} !!!`
-    if (check[0].checked) {
-
-        caixa = 'Futebol'
-
-        
-
-    } 
+window.alert(`Oi boa noite!`)
 }
