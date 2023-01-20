164ebfdaba1aea2fc87136dc118590a766ac9691
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-01-20 11:34:37 -0300

    atualizado

---
+++ curso-em-video/aula14/index.js
@@ -0,0 +1,16 @@
+
+
+
+let c = 1
+ while (c <= 6) {
+
+    console.log('Tudo Bem!')
+ }
+
+/* console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!') */
