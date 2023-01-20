3354403e529a128a83e0b16642d493f9efea432d
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-01-20 12:26:16 -0300

    atualizado while do for

--- curso-em-video/aula14/index.js
+++ curso-em-video/aula14/index.js
@@ -1,16 +1,23 @@
 
-
+// WHILE = ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO INICIO
+// DO  = ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO FINAL
 
 let c = 1
- while (c <= 6) {
 
-    console.log('Tudo Bem!')
- }
+do { // DO = faça
 
-/* console.log('Tudo Bem!')
-console.log('Tudo Bem!')
-console.log('Tudo Bem!')
-console.log('Tudo Bem!')
-console.log('Tudo Bem!')
-console.log('Tudo Bem!')
-console.log('Tudo Bem!') */
+   console.log(`Passo ${c}`)
+   c++ // c = c + 1
+
+} while (c <= 6) // while = enquanto
+
+
+
+/*
+let c = 1
+
+ while (c <= 6) { // while = enquanto
+
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+*/
---
+++ curso-em-video/aula14/while.js
@@ -0,0 +1,18 @@
+
+
+// Estrutura  (FOR)  é dividida em 3 partes (INICIO; TESTE LOGICO; INCREMENTO)
+
+
+
+let c = 1
+
+/* while (c <= 10) {
+
+    console.log (`Recebendo ${c}`)
+    c++
+} */
+
+for (let c = 1; c <= 10; c++) {
+
+    console.log (`Recebendo ${c}`)
+}
