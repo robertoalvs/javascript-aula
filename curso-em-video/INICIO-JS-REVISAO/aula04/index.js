db1ada568369801facff20f92b9a914c268e7f62
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-02-14 14:21:46 -0300

    atualização

--- curso-em-video/INICIO-JS-REVISAO/aula04/index.js
+++ curso-em-video/INICIO-JS-REVISAO/aula04/index.js
@@ -1,3 +1,12 @@
+
+/*
 alert('Olá Mundo!!!')
 confirm('Está gostando de javascript?')
 prompt('Qual é seu nome')
+*/
+let n = 2
+
+n = ''
+nn = Number(n.value)
+
+console.log(typeof n)
---
+++ curso-em-video/INICIO-JS-REVISAO/aula06/index.js
@@ -0,0 +1,9 @@
+
+alert('Olá Jovem!!!')
+let conf = confirm('Está gostando de javascript?')
+let nome = prompt('Qual é seu nome')
+let tipo = console.log(typeof nome, typeof conf)
+alert(`É um prazer te conhecer ${nome}`)
+console.log(nome)
+console.log(conf)
+console.log(tipo)
