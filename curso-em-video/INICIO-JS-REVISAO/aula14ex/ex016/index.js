9866b9dc3d1a3c93bb2195a1b8901e9e550c5547
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-03-10 14:28:10 -0300

    atualizado

--- curso-em-video/INICIO-JS-REVISAO/aula14ex/ex016/index.js
+++ curso-em-video/INICIO-JS-REVISAO/aula14ex/ex016/index.js
@@ -3,55 +3,42 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-    
     let res = document.getElementById('res')
-    let num = Number(ini.value)
-    let num1 = Number(fim.value)
-    let num2 = Number(passo.value)
 
-    if (ini.value.length == 0 || fim.value.length || passo.value.length == 0 ) {
+    let num1 = Number(ini.value)
+    let num2 = Number(fim.value)
+    let num3 = Number(passo.value)
 
-        res.innerHTML = 'IMPOSSÍVEL CONTAR:'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
 
+        alert('ERRO! Digite um Número!')
 
+    } else {
 
-    }else {
+       res.innerHTML = 'Contando... <br>'
+        if ( num3 <= 0) {
 
-        res.innerHTML += 'Contando:'
-
-        if (num2 <= 0) {
-            
-            num2 = 1
             alert('Passo inválido! Considerando PASSO 1')
-            
-
+            num3 =1
         }
 
-        if (num < num1) {
+       if ( num1 < num2) {
 
-            // Contagem Crescente
-            for (let c = num; c <= num1; c += num2) {
+        for (let c = num1; c <= num2; c += num3) {
 
-                res.innerHTML += `${c} \u{1F449}`
-            }
+            res.innerHTML += `${c} \u{1F449}`
+           }
+    
+       }else {
 
-        }else {
+        for (let c = num1; c >= num2; c -= num3 ) {
 
-            // Contagem regressiva
-            for (let c = num; c >= num1; c -= num2) {
-
-                res.innerHTML += `${c} \u{1F449}`
-            }
-        }
+            res.innerHTML += `${c} \u{1F449}`
+         }
+          
+       }
         res.innerHTML += `\u{1F3C1}`
+       
+       
     }
-        
-
-
-        
-
-
-
-
-
 }
